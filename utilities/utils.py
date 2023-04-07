2009ad42593143b5bd33b531ffecbafba96f4c39
Liam M <twoutrigger@outlook.com>
2023-04-07 11:57:39 -0400

    rock_paper_scissors function

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,3 +1,11 @@
+import random
 
-## add random generator
-## rerun in case of tie
+def rock_paper_scissors(x):
+
+    # need to implement game logic
+    # use while statement to rerun in case of tie
+
+    return random.randrange(0, 3, 1) 
+
+for i in range(0, 3, 1):
+    print(i)
